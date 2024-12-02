6b2ac9856a98c3ad60e89dafdf3c7a64e7a21ded
Angela <ms.yumadilowa@mail.ru>
2024-12-02 21:11:50 +0300

    build folder

---
+++ precache-manifest.0975b8c69c4e32770da23732feb228a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf903307c4bf6e25ca3453b76e5f9a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2454d5d978337343c46",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "e2454d5d978337343c46",
+    "url": "/static/js/main.f8e4e133.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
