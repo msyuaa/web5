9fdf3cf9781e1818cc042269e9995959f74a3417
Angela <ms.yumadilowa@mail.ru>
2024-12-02 21:43:58 +0300

    build folder

---
+++ precache-manifest.4e22e8e0bd9af35c025134527202d84a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b481886dc5d74ab1659f930fff48237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f70eb848b1f9c0064877",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa2d8ee02cf184fc4314",
+    "url": "/static/js/2.3ad75f56.chunk.js"
+  },
+  {
+    "revision": "f70eb848b1f9c0064877",
+    "url": "/static/js/main.670bd95e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83e4693019fc8e5fca6ba5d073cdd109.js"
+  "/precache-manifest.4e22e8e0bd9af35c025134527202d84a.js"
 );
 
 self.addEventListener('message', (event) => {
